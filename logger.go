2be493bf738609127b170c42540ddee1d37360f2
houser <houserqu@qq.com>
2022-09-08 14:23:18 +0800

    feat: 优化日志配置

--- logger.go
+++ logger.go
@@ -1,6 +1,7 @@
 package ginc
 
 import (
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -23,36 +24,43 @@ func InitLogger() {
 	Logger.SetLevel(logrus.DebugLevel) // 日志输出级别
 	Logger.Out = ioutil.Discard        // 禁止 logrus 的输出
 
-	// 配置文件输入 hook
-	logFolderPath, err := filepath.Abs(viper.GetString("log.path"))
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+	var writer io.Writer // 输出位置
 
-	// 如果日志目录不存在则创建
-	_, err = os.Stat(logFolderPath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			if err = os.Mkdir(logFolderPath, os.ModePerm); err != nil {
-				log.Fatalf("create log file err: %v", errors.WithStack(err))
-			}
-
-		} else {
-			log.Fatal(err.Error())
+	if viper.GetString("log.out") == "file" {
+		// 配置文件输入 hook
+		logFolderPath, err := filepath.Abs(viper.GetString("log.path"))
+		if err != nil {
+			log.Fatalf(err.Error())
 		}
-	}
 
-	logPath := path.Join(logFolderPath, "default.log")
+		// 如果日志目录不存在则创建
+		_, err = os.Stat(logFolderPath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				if err = os.Mkdir(logFolderPath, os.ModePerm); err != nil {
+					log.Fatalf("create log file err: %v", errors.WithStack(err))
+				}
 
-	writer, err := rotatelogs.New(
-		logPath+".%Y-%m-%d.log",
-		rotatelogs.WithLinkName(logPath),          // 生成软链，指向最新日志文件
-		rotatelogs.WithMaxAge(14*24*time.Hour),    // 文件最大保存时间
-		rotatelogs.WithRotationTime(24*time.Hour), // 日志切割时间间隔
-	)
+			} else {
+				log.Fatal(err.Error())
+			}
+		}
 
-	if err != nil {
-		log.Fatalf("config log file system logger error. %v", errors.WithStack(err))
+		logPath := path.Join(logFolderPath, "default.log")
+
+		writer, err = rotatelogs.New(
+			logPath+".%Y-%m-%d.log",
+			rotatelogs.WithLinkName(logPath),          // 生成软链，指向最新日志文件
+			rotatelogs.WithMaxAge(14*24*time.Hour),    // 文件最大保存时间
+			rotatelogs.WithRotationTime(24*time.Hour), // 日志切割时间间隔
+		)
+
+		if err != nil {
+			log.Fatalf("config log file system logger error. %v", errors.WithStack(err))
+		}
+	} else {
+		// 默认输出到控制台
+		writer = os.Stdout
 	}
 
 	// 不同级别输出
